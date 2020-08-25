887713fbcb047ab9192d82853d47e7369319b465
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-25 09:32:03 +0600

    reduce instead of fo loop

--- src/components/CountryAdded/CountryAdded.js
+++ src/components/CountryAdded/CountryAdded.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const CountryAdded = (props) => {
     const countries = props.country;
-    let totalPopulation = 0;
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
-        totalPopulation = totalPopulation + country.population;
+    // let totalPopulation = 0;
+    // for (let i = 0; i < countries.length; i++) {
+    //     const country = countries[i];
+    //     totalPopulation = totalPopulation + country.population;
         
-    }
-    
+    // }
+    const totalPopulation = countries.reduce((sum, country) => sum + country.population ,0)
+
     return (
         <div>
             <h2>Information about country: {countries.length}</h2>
