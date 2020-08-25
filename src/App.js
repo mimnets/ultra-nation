7c4a2d101bfcd77b02158f8be0001a9f4a268513
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-25 07:36:59 +0600

    ultra nation first call

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data=>setCountries(data))
+    // .then(data=>console.log(data))
+  }, [])
+  
+  return (
+    <div className="App">
+      <h1>Country loaded: {countries.length}</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
