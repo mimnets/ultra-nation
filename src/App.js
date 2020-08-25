293ae16c97c177074bf0512a7997d027a6255aea
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-25 08:15:43 +0600

    call wtih props as child componenet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Country from './components/Country/Country';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -23,7 +24,7 @@ function App() {
       <h1>Country loaded: {countries.length}</h1>
       <ul>
         {
-          countries.map(country => <li>{country.name}</li>)
+          countries.map(country => <Country name ={country.name}></Country>)
         }
       </ul>
       <header className="App-header">
